979fe29157205b7d963be808cde4a34b27cf69c3
miclemabasie <miclemabasie3@gmail.com>
2023-05-05 22:36:28 +0100

    completed functionality for cart

--- src/cart/views.py
+++ src/cart/views.py
@@ -14,7 +14,9 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        cart.add(product=product, quantity=cd["quantity"])
+        cart.add(
+            product=product, quantity=cd["quantity"], override_quantity=cd["override"]
+        )
 
     return redirect("cart:cart_detail")
 
@@ -29,6 +31,10 @@ def cart_remove(request, product_id):
 
 def cart_detail(request):
     cart = Cart(request)
+    for item in cart:
+        item["update_quantity_form"] = CartAddProductForm(
+            initial={"quantity": item["quantity"], "override": True}
+        )
     template_name = "cart/detail.html"
     context = {
         "cart": cart,
